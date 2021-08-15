d0499320223e011ad3a2280ac5f799b82de38655
Marmik Patel <marmikpatel250@gmail.com>
2021-08-15 17:43:41 +0530

    Added API Documentation - ReDoc

--- quicksaloon/urls.py
+++ quicksaloon/urls.py
@@ -3,20 +3,37 @@ from django.contrib import admin
 from django.urls import path, include
 
 from quicksaloon import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Quicksaloon API",
+        default_version='1.0.0',
+        description="Quicksaloon API, created by <a href='https://thedataboy.com/'>TheDataBoy</a> with "
+                    "<a href='https://4itsol.com/'>4IT Solutions</a>",
+        contact=openapi.Contact(email="marmikpatel250@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('accounts/', include('allauth.urls')),
-    path('superadmin', include('superadmin.urls')),
-    path('', include('public.urls')),
+                  path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    # API URLS
-    path('api/', include('public.api.urls')),
-    path('superadmin/api/', include('superadmin.api.urls')),
-    path('shopadmin/api/', include('shopadmin.api.urls')),
-    path('customer/api/', include('customer.api.urls')),
+                  path('admin/', admin.site.urls),
+                  path('superadmin', include('superadmin.urls')),
+                  # path('', include('public.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
+                  # API URLS
+                  path('api/', include('public.api.urls')),
+                  path('superadmin/api/', include('superadmin.api.urls')),
+                  path('shopadmin/api/', include('shopadmin.api.urls')),
+                  path('customer/api/', include('customer.api.urls')),
+                  path('barber/api/', include('barber.api.urls')),
+
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
     settings.STATIC_URL,
     document_root=settings.STATIC_ROOT
 )
