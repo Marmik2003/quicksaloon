19a626e43ceee9fb1ab79436a92598a797547d03
Marmik Patel <marmikpatel250@gmail.com>
2021-11-20 16:43:00 +0530

    bugs fixed

--- quicksaloon/settings.py
+++ quicksaloon/settings.py
@@ -29,7 +29,7 @@ PG_DATABASE_PASSWORD = env('PG_DATABASE_PASSWORD')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u%^-rd#ye(^m=1))u9gpuij_lq4ccv_iwvav%6%ho2b#=yocw+'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -197,6 +197,6 @@ LOGIN_URL = r'/'
 CORS_ALLOWED_ORIGINS = [
     "https://quicksaloon.com",
     "https://quicksaloon.thedataboy.com",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
